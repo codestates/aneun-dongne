73b331c613800f6120f985668211614197fb6aa0
haeyong9701 <haeyong9701@gmail.com>
2021-11-29 23:17:27 +0900

    modify App.js 및 서버 index.js 수정

--- server/index.js
+++ server/index.js
@@ -1,18 +1,23 @@
 const express = require("express");
-const cors = require('cors');
+const cors = require("cors");
 const app = express();
 app.use(express.json());
 const port = 80;
 
 app.use(
   cors({
-    origin: true,
-    credentials: true
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    credentials: true,
   })
 );
 
-app.get("/api", (req, res) => {
-  res.send({ hello : "Hello World!" });
+app.get("/", (req, res) => {
+  res.status(201).send("Hello World");
+});
+
+app.get("/hello", (req, res) => {
+  res.json({ data: "서버랑 연결 됐어요!!" });
 });
 
 app.listen(port, () => {

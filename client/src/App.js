568e7f791feef8cf5150c058b96d75fa0e57b259
haeyong9701 <haeyong9701@gmail.com>
2021-11-30 00:18:40 +0900

    modify 클라이언트 App.js 수정

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
 
-function App() {
+const App = () => {
+  const [isContent, setContnet] = useState("연결됐니?");
+
+  const isToggle = () => {
+    axios.get("http://localhost:80/hello").then((res) => {
+      // console.log(res.data.data);
+      setContnet(res.data.data);
+    });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={isToggle}>{isContent}</button>
     </div>
   );
-}
+};
 
 export default App;

1df92feca248e12bef3df76cb4712413d19c945c
haeyong9701 <haeyong9701@gmail.com>
2021-11-30 16:04:01 +0900

    feat 로그인 함수 추가

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,45 @@
 import React, { useState } from "react";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import axios from "axios";
 import "./App.css";
 
 import Mainpage from "./pages/Mainpage";
+import Home from "./pages/Home";
 
 const App = () => {
+  const [isLogin, setIsLogin] = useState(false);
+  const [userinfo, setUserinfo] = useState(null);
+
+  const history = useHistory();
+
+  const isAuthenticated = () => {
+    axios.get(`${process.env.REACT_APP_API_URL}/user/info`).then((res) => {
+      setUserinfo(res.data.data.userInfo);
+      setIsLogin(true);
+      history.push("/");
+    });
+  };
+
+  const handleResponseSuccess = () => {
+    isAuthenticated();
+  };
+
   return (
     <>
-      <Mainpage />
+      <BrowserRouter>
+        <Switch>
+          <Route path="/main">
+            <Mainpage handleResponseSuccess={handleResponseSuccess} />
+          </Route>
+          <Route exact path="/home">
+            <Home userinfo={userinfo} />
+          </Route>
+          <Route path="/">
+            {isLogin ? <Redirect to="/home" /> : <Redirect to="/main" />}
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </>
   );
 };
--- client/src/components/ModalLogin.js
+++ client/src/components/ModalLogin.js
@@ -1,7 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
+import axios from "axios";
 
-const ModalLogin = () => {
-  return <div>로그인</div>;
+export const FormContainer = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+
+  input {
+    margin: 20px 0px;
+    width: 300px;
+    height: 30px;
+  }
+
+  .login-button {
+    cursor: pointer;
+    width: 60px;
+    height: 40px;
+    background-color: #a3dcf3;
+  }
+
+  .signup-link {
+    cursor: pointer;
+    color: #a3dcf3;
+  }
+`;
+
+const ModalLogin = ({ handleResponseSuccess }) => {
+  const [loginInfo, setLoginInfo] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleInputValue = (key) => (e) => {
+    setLoginInfo({ ...loginInfo, [key]: e.target.value });
+  };
+
+  const handleLogin = () => {
+    const { email, password } = loginInfo;
+
+    axios
+      .post(
+        `${process.env.REACT_APP_API_URL}/user/login`,
+        {
+          email,
+          password,
+        },
+        { "Content-Type": "application/json", withCredentials: true }
+      )
+      .then(() => {
+        handleResponseSuccess();
+      });
+  };
+
+  return (
+    <>
+      <FormContainer>
+        <div>Log in</div>
+        <form onSubmit={(e) => e.preventDefault()}>
+          <label for="email">Email</label>
+          <input id="email" type="text" onChange={handleInputValue("email")} />
+          <label for="password">password</label>
+          <input
+            id="password"
+            type="password"
+            onChange={handleInputValue("password")}
+          />
+          <div className="login-button" onClick={handleLogin}>
+            Log in
+          </div>
+          <div>아직 회원이 아니신가요?</div>
+          <div className="signup-link">회원가입하기</div>
+        </form>
+      </FormContainer>
+    </>
+  );
 };
 
 export default ModalLogin;

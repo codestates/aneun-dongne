d2dcfa2ae892f7a2ea0d15691637165cbc571f4a
남경화 <hwahwa7879@gmail.com>
2021-12-06 14:13:35 +0900

    add 메인페이지 이미지 수정

--- client/src/pages/Mypage/userinfo.js
+++ client/src/pages/Mypage/userinfo.js
@@ -10,11 +10,12 @@ export const Body = styled.div`
   position: relative;
   display: flex;
   flex-direction: column;
+  margin-top: 80px;
 `;
 
 export const userInfopage = styled.div`
   position: fixed;
-  top: 0;
+  top: 8%;
   left: 0;
   width: 100%;
   height: 100%;

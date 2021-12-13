20b4a5b06d4441e0a0daea640670dcbf7dc7f15a
남경화 <hwahwa7879@gmail.com>
2021-12-13 19:55:13 +0900

    add 마이페이지 수정 02

--- client/src/components/MyLike/MyLike.js
+++ client/src/components/MyLike/MyLike.js
@@ -1,43 +1,65 @@
 import React from "react";
+import { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
-const [like, setLike] = useState(0);
-const [likeOrNot, setLikeOrNot] = useState(false);
-
-//로그인 된 상태에서
-//좋아요 게시글 불러오기 (홈에서 state를 변경했음)
-//게시글을 누르면 onClick={() => history.push(/mypage/likelists/${like_post_contentid.id})}
-//세부정보 페이지에서 좋아요 한 게시물을 가져오기
-//좋아요 리스트가 없다면 -> 좋아요 한 게시글이 없습니다.
-
-// useEffect(() => {
-//   axios
-//     .get(`${process.env.REACT_APP_API_URL}/like/${contentId}`, { withCredentials: "true" })
-//     .then((res) => {
-//       const like = { likeOrNot: res.data.data.isLiked, likeCount: res.data.data.likeCount };
-//       setLike(like.likeCount);
-//       setLikeOrNot(like.likeOrNot);
-//     })
-//     .then((res) => {
-//       setLikeLoading(false);
-//     });
-// }, []);
+import axios from "axios";
+// const [like, setLike] = useState(0);
 
 const MyLike = () => {
+  const [userInfo, setUserInfo] = useState({});
+  const [img, setImg] = useState(""); //post_firstimage -> 관광지 사진
+  const [areacode, setAreacode] = useState(""); // post_areacode: 1  ->위치표기 함수 있음.
+  const [title, setTitle] = useState(""); // post_title -> 올림픽공원
+
+  const [likeCount, setLikeCount] = useState(false);
+
+  const [like, setLike] = useState(0); //DetailPage에서 일단 가져옴
+  const [likeOrNot, setLikeOrNot] = useState(false); //DetailPage에서 일단 가져옴
+  const [placeLocation, setPlaceLocation] = useState({ lon: 0, lat: 0 }); //위치 정보
+
   const history = useHistory();
+  //포스트 정보가 들어오는 곳
+  const handleLikePage = () => {
+    axios
+      .get(`${process.env.REACT_APP_API_URL}/mypage/likelists/${contentId}`, {
+        withCredentials: "true",
+      })
+      .then((res) => {
+        console.log("정보가 들어오면 확인하자", res.data);
+        setImg(res.data.post.post_firstimage);
+        setAreacode(res.data.post_areacode);
+        setTitle(res.data.post.post_title);
+        setLikeCount(res.data.Like.likeCount);
+      });
+    // .then((res) => {});
+  };
+
+  //좋아요 버튼을 해지
+  useEffect(() => {
+    axios
+      .delete(`${process.env.REACT_APP_API_URL}/like/${contentId}`, {
+        withCredentials: "true",
+      })
+      .then((res) => {
+        const like = { likeOrNot: res.data.data.isLiked, likeCount: res.data.data.likeCount };
+        console.log("정보가 들어오면 확인하자", res.data);
+        setLike(like.likeCount);
+        setLikeOrNot(like.likeOrNot);
+      });
+  }, []);
+
+  useEffect(() => {
+    handleLikePage();
+  }, []);
 
   return (
     <div id="like-list">
-      {!setLike === 0 ? (
+      {!userInfo === false ? ( //userinfo like가 없는경우에
         <div id="like-list-text">좋아요 한 게시글이 없습니다.</div>
       ) : (
-        <div
+        <MyLikelist
           id="content-like-list"
           onClick={() => history.push(`${process.env.REACT_APP_API_URL}/mypage/likelists/${like_post_contentid.id}`)}
-        >
-          {setLike.map((ele) => {
-            return <LikeList Likelist={ele.Likelist} />;
-          })}
-        </div>
+        ></MyLikelist>
       )}
     </div>
   );

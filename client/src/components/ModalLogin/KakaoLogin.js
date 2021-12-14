b7d3010e3c48b8b3d424ba569ca5a074ab51b5c9
june <66232436+ryu9663@users.noreply.github.com>
2021-12-14 12:39:34 +0900

    Update KakaoLogin.js

--- client/src/components/ModalLogin/KakaoLogin.js
+++ client/src/components/ModalLogin/KakaoLogin.js
@@ -4,7 +4,7 @@ import axios from "axios";
 function KakaoLogin() {
   const client_id = process.env.REACT_APP_REST_API || "6d44f5b3a39f09658ad4d72515a788d4";
   const redirect_uri = process.env.REACT_APP_REDIRECT_URI || "https://localhost:3000";
-  const KAKAO_LOGIN_URL = `https://kauth.kakao.com/oauth/authorize?client_id=6d44f5b3a39f09658ad4d72515a788d4&redirect_uri=https://localhost:3000&response_type=code`;
+const KAKAO_LOGIN_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${client_id}&redirect_uri=https://localhost:3000&response_type=code`;
 
   function requestAssign() {
     window.location.assign(KAKAO_LOGIN_URL);

05bc0c247992f438ed388085980d89b8cb9e8f11
Son SeungYi <sonsiee2@gmail.com>
2021-12-15 03:50:00 +0900

    add : server commit

--- server/controllers/users/kakaoCallback.js
+++ server/controllers/users/kakaoCallback.js
@@ -8,10 +8,10 @@ module.exports = async (req, res) => {
       `https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id=${process.env.KAKAO_REST_API_KEY}&redirect_uri=${process.env.CALLBACK_URL}&client_secret=${process.env.CLIENT_SECRET}&code=${req.query.code}`,
       { headers: { "Content-type": "application/x-www-form-urlencoded;charset=utf-8" } }
     ) //토큰 요청하는 url
-    .then((response) => {
-      console.log(response.data);
+    .then((tokendata) => {
+      console.log(tokendata.data);
       axios
-        .get(`https://kapi.kakao.com/v2/user/me?access_token=${response.data.access_token}`, {
+        .get(`https://kapi.kakao.com/v2/user/me?access_token=${tokendata.data.access_token}`, {
           headers: { "Content-type": "application/x-www-form-urlencoded;charset=utf-8" },
         }) //토큰을 주면 유저정보를 주는 url
         .then((response) => {
@@ -29,32 +29,54 @@ module.exports = async (req, res) => {
               provider: "kakao",
             }, //이메일을 기준으로 유저가 db에 없으면 새로 생성한다, 이미지는 기존 카톡 프로필이미지로
           }).then(([save, created]) => {
-            // if (!created) {
-            //   User.update(
-            //     {
-            //       nickname: response.data.kakao_account.profile.nickname,
-            //       user_image_path: response.data.kakao_account.profile.profile_image_url,
-            //       user_thumbnail_path: response.data.kakao_account.profile.thumbnail_image_url,
-            //     },
-            //     {
-            //       where: {
-            //         email: response.data.kakao_account.email,
-            //       },
-            //     }
-            //   );
-            // }  // 카톡 프사, 닉네임 등등이 바뀌면 여기도 자동으로 바뀔지 모르겠음
-            delete save.dataValues.password;
-            const accessToken = generateAccessToken(save.dataValues);
-            res.cookie("jwt", accessToken, {
-              // maxAge: 1000 * 60 * 60 * 24 * 7,
-              // domain: ".aneun-dongne.com", (배포)
-              path: "/",
-              secure: true,
-              sameSite: "None",
-            }); //토큰 담은 쿠키 전달
-            res.redirect(`${process.env.URL_AFTER_LOGIN}`); //로그인 후 이동할 페이지 : MAIN_URL
-            // sendAccessToken(res, accessToken);
-            // 로그인 인증 완료
+            if (!created) {
+              User.update(
+                {
+                  nickname: response.data.kakao_account.profile.nickname,
+                  user_image_path: response.data.kakao_account.profile.profile_image_url,
+                  user_thumbnail_path: response.data.kakao_account.profile.thumbnail_image_url,
+                },
+                {
+                  where: {
+                    email: response.data.kakao_account.email,
+                  },
+                }
+              ).then(() => {
+                User.findOne({
+                  where: {
+                    email: response.data.kakao_account.email,
+                  },
+                }).then((data) => {
+                  console.log(data);
+                  delete data.dataValues.password;
+                  const accessToken = generateAccessToken(data.dataValues);
+                  res.cookie("jwt", accessToken, {
+                    // maxAge: 1000 * 60 * 60 * 24 * 7,
+                    // domain: ".aneun-dongne.com", (배포)
+                    path: "/",
+                    secure: true,
+                    sameSite: "None",
+                  }); //토큰 담은 쿠키 전달
+                  res.redirect(`${process.env.URL_AFTER_LOGIN}`); //로그인 후 이동할 페이지 : MAIN_URL
+                  // sendAccessToken(res, accessToken);
+                  // 로그인 인증 완료});
+                });
+              });
+            } // 카톡 프사, 닉네임 등등이 바뀌면 여기도 자동으로 바뀌어야 해서
+            else {
+              delete save.dataValues.password;
+              const accessToken = generateAccessToken(save.dataValues);
+              res.cookie("jwt", accessToken, {
+                // maxAge: 1000 * 60 * 60 * 24 * 7,
+                // domain: ".aneun-dongne.com", (배포)
+                path: "/",
+                secure: true,
+                sameSite: "None",
+              }); //토큰 담은 쿠키 전달
+              res.redirect(`${process.env.URL_AFTER_LOGIN}`); //로그인 후 이동할 페이지 : MAIN_URL
+              // sendAccessToken(res, accessToken);
+              // 로그인 인증 완료
+            }
           });
         });
     })

314a5d371b0266b49092d2049c79b16a2d244402
haeyong9701 <haeyong9701@gmail.com>
2021-12-17 20:44:16 +0900

    style 홈화면

--- client/src/components/PlaceCard/PlaceCards.js
+++ client/src/components/PlaceCard/PlaceCards.js
@@ -16,27 +16,6 @@ const PlaceCard = styled.div`
   box-shadow: 4px 4px 4px rgb(85, 85, 85);
   transition: box-shadow 0.1s, transform 0.1s;
   text-decoration: inherit;
-  animation: color-change3 2s infinite;
-  @keyframes color-change3 {
-    0% {
-      /* border-left: #c1ff6b 3px solid; */
-
-      /* border-top: #c1ff6b 3px solid; */
-      border: #c1ff6b 3px solid;
-    }
-    50% {
-      /* border-left: #fab4b4 3px solid; */
-
-      /* border-top: #fab4b4 3px solid; */
-      border: #fab4b4 3px solid;
-    }
-    100% {
-      /* border-left: #46ffff 3px solid; */
-
-      /* border-top: #46ffff 3px solid; */
-      border: #46ffff 3px solid;
-    }
-  }
   &:hover {
     transform: scale(1.1);
     box-shadow: inset 2px 2px 2px 0px rgba(255, 255, 255, 0.5), 7px 7px 20px 0px rgba(0, 0, 0, 0.1),

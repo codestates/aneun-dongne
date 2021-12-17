f7c909cbe7e53aa8b6078b102889580313cc8465
남경화 <hwahwa7879@gmail.com>
2021-12-18 04:40:15 +0900

    add 반응형 추가

--- client/src/pages/MainPage/MainPage.js
+++ client/src/pages/MainPage/MainPage.js
@@ -266,85 +266,63 @@ function MainPage() {
   console.log("로긴되었나요", isLogin);
 
   return (
-    <>
-      <Styled.Body>
-        <Styled.MainTitleView>
-          <img src="/images/look.gif" muted autoPlay loop playsInline />
-          <div className="main-title">
-            <div className="title">어디론가 떠나고 싶으신가요?</div>
-            <Styled.StartButton onClick={ToHome}>시작하기</Styled.StartButton>
-            <div className="icons">{<Icon size={"100"} icon={angleDoubleDown} onClick={ToScrollBottom} />}</div>
-          </div>
-        </Styled.MainTitleView>
-
-        <Styled.PopularTitleView className="populartitle" data-aos="fade-up" data-aos-duration="1000">
-          <div className="title">우리 동네에서 인기있는 관광지는?</div>
-
-          <Styled.VideoContainer>
-            <Styled.Image>
-              <img src="images/dongnea.png" />
-
-              <img
-                className="play"
-                src="https://res.cloudinary.com/cloudinary/image/upload/c_limit,w_770/f_auto,fl_lossy,q_auto/Mario_1.gif"
-                muted
-                autoPlay
-                loop
-                playsInline
-              />
-            </Styled.Image>
-          </Styled.VideoContainer>
-        </Styled.PopularTitleView>
-
-        <Styled.FocusTitleView className="focustitle" data-aos="fade-up" data-aos-duration="1000">
-          <div className="title">저희는 여러분의 관심사에 알맞는 관광지를 찾아드릴 수 있습니다.</div>
-          <Styled.VideoContainer>
-            <Styled.Image>
-              <img
-                className="play"
-                src="https://res.cloudinary.com/cloudinary/image/upload/c_limit,w_770/f_auto,fl_lossy,q_auto/Mario_1.gif"
-                muted
-                autoPlay
-                loop
-                playsInline
-              />
-              <img src="images/mapimgpeople.png" />
-            </Styled.Image>
-          </Styled.VideoContainer>
-        </Styled.FocusTitleView>
-
-        <Styled.DiyTitleView className="diytitle" data-aos="fade-up" data-aos-duration="1000">
-          <div className="title">그곳이 어디라도 간직하고 싶다면 내가 서있는 바로 그곳을 저장할 수 있어요.</div>
-          <Styled.VideoContainer>
-            <Styled.Image>
-              <img src="images/likeimg.png" />
-
-              <img
-                className="play"
-                src="https://res.cloudinary.com/cloudinary/image/upload/c_limit,w_770/f_auto,fl_lossy,q_auto/Mario_1.gif"
-                muted
-                autoPlay
-                loop
-                playsInline
-              />
-            </Styled.Image>
-          </Styled.VideoContainer>
-        </Styled.DiyTitleView>
-
-        <Styled.PeopleTitleView className="peopletitle" data-aos="fade-up" data-aos-duration="1000">
-          <div className="title" data-aos="fade-up" data-aos-duration="1000">
-            유저들의 후기
-          </div>
-          <Review />
-        </Styled.PeopleTitleView>
-
-        <Styled.TitleEndView>
-          <div className="title">나와 어울리는 장소로 떠날 준비가 되셨나요?</div>
+    <Styled.Body>
+      <Styled.MainTitleView>
+        <img src="/images/look.gif" muted autoPlay loop playsInline />
+        <div className="main-title">
+          <div className="title">어디론가 떠나고 싶으신가요?</div>
           <Styled.StartButton onClick={ToHome}>시작하기</Styled.StartButton>
-        </Styled.TitleEndView>
-        <Footer />
-      </Styled.Body>
-    </>
+          <div className="icons">{<Icon size={"80"} icon={angleDoubleDown} onClick={ToScrollBottom} />}</div>
+        </div>
+      </Styled.MainTitleView>
+
+      <Styled.PopularTitleView className="populartitle" data-aos="fade-up" data-aos-duration="1000">
+        <div className="title">우리 동네에서 인기있는 관광지는?</div>
+
+        <Styled.VideoContainer>
+          <Styled.Image>
+            <img src="images/mapclick.png" />
+
+            <img className="play-popular" src="/images/location.gif" muted autoPlay loop playsInline />
+          </Styled.Image>
+        </Styled.VideoContainer>
+      </Styled.PopularTitleView>
+
+      <Styled.FocusTitleView className="focustitle" data-aos="fade-right" data-aos-duration="1000">
+        <div className="title">여러분의 관심사에 알맞는 관광지를 찾아드릴 수 있어요.</div>
+        <Styled.VideoContainer>
+          <Styled.Image>
+            <img className="play-focus" src="/images/location.gif" muted autoPlay loop playsInline />
+            <img src="images/mapimgpeople.png" />
+          </Styled.Image>
+        </Styled.VideoContainer>
+      </Styled.FocusTitleView>
+
+      <Styled.DiyTitleView className="diytitle" data-aos="fade-left" data-aos-duration="1000">
+        <div className="title">그곳이 어디라도 간직하고 싶다면 내가 서있는 바로 그곳을 저장할 수 있어요.</div>
+        <Styled.VideoContainer>
+          <Styled.Image>
+            <img src="images/likeimg.png" />
+
+            <img className="play-diy" src="/images/location.gif" muted autoPlay loop playsInline />
+          </Styled.Image>
+        </Styled.VideoContainer>
+      </Styled.DiyTitleView>
+
+      <Styled.PeopleTitleView className="peopletitle" data-aos="fade-up" data-aos-duration="1000">
+        <div className="title" data-aos="fade-up" data-aos-duration="1000">
+          유저들의 후기
+        </div>
+        <Review />
+      </Styled.PeopleTitleView>
+
+      <Styled.TitleEndView>
+        <Styled.StartButton onClick={ToHome}>시작하기</Styled.StartButton>
+        <div className="title">나와 어울리는 장소로 떠날 준비가 되셨나요?</div>
+      </Styled.TitleEndView>
+
+      <Footer />
+    </Styled.Body>
   );
 }
 export default MainPage;
--- client/src/pages/MainPage/style.js
+++ client/src/pages/MainPage/style.js
@@ -3,245 +3,17 @@ import styled from "styled-components";
 export const Styled = {
   Body: styled.div`
     position: relative;
-    margin-right: auto;
-    margin-left: auto;
-    /* overflow-x: visible; */
+    width: 100%;
+    overflow-x: hidden;
+
+    /* overflow-x: hidden;  */
+    /* margin-right: auto;
+    margin-left: auto; */ /* overflow-x: visible; */
     /* display: flex;
-flex-direction: column; */
-    @media all and (min-width: 768px) and (max-width: 1050px) {
-      flex-direction: column;
-      margin: 0 auto;
-      padding: 0 2vw;
+    flex-direction: column; */
 
-      width: 60%;
-      height: 100%;
-    }
-
-    @media only screen and (min-width: 768px) {
-    }
-
-    @media screen and (min-width: 480px) and (max-width: 767px) {
-    }
-  `,
-  StartButton: styled.button`
-    /* position: absolute; */
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    font-size: 1.2rem;
-    color: white;
-    width: 180px;
-    height: 70px;
-    border-style: none;
-    cursor: pointer;
-    transition: all 0.3s;
-    /* margin-left: 350px;
-  margin-right: 350px; */
-    border: 1px solid rgb(194, 194, 194);
-    border-radius: 5px;
-    margin-top: 20px;
-    margin-right: auto;
-    margin-left: auto;
-    background-color: transparent;
-
-    :hover {
-      background-color: #6af4aa;
-      transition: all 0.3s;
-    }
-
-    @media all and (min-width: 768px) and (max-width: 1050px) {
-      margin-top: 10rem;
-      flex-direction: column;
-      margin: 0 auto;
-
-      border: 1px solid #aaa;
-      width: 50%;
-      height: 50%;
-      font-size: 0.5rem;
-    }
-
-    @media only screen and (min-width: 768px) {
-    }
-
-    @media screen and (min-width: 480px) and (max-width: 767px) {
-    }
-  `,
-  PopularTitleView: styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-direction: column;
-    height: 100vh;
-    margin-top: 10%;
-
-    .title {
-      font-size: 2.5rem;
-      font-weight: bold;
-      @media all and (min-width: 768px) and (max-width: 1050px) {
-        margin-top: 30px;
-        font-size: 0.8rem;
-      }
-    }
-
-    @media all and (min-width: 768px) and (max-width: 1050px) {
-      flex-direction: column;
-      margin-top: 3px;
-      margin: 0 auto;
-      width: 100%;
-      height: 50%;
-    }
-  `,
-
-  FocusTitleView: styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-direction: column;
-    height: 100vh;
-
-    .title {
-      font-size: 2.5rem;
-      font-weight: bold;
-      @media all and (min-width: 768px) and (max-width: 1050px) {
-        margin-top: 30px;
-        font-size: 0.8rem;
-      }
-    }
-    @media all and (min-width: 768px) and (max-width: 1050px) {
-      flex-direction: column;
-      margin-top: 3px;
-      margin: 0 auto;
-      width: 100%;
-      height: 50%;
-    }
-  `,
-  DiyTitleView: styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-direction: column;
-    height: 100vh;
-
-    .title {
-      font-size: 2.5rem;
-      font-weight: bold;
-      @media all and (min-width: 768px) and (max-width: 1050px) {
-        margin-top: 20px;
-        font-size: 0.8rem;
-      }
-    }
-    @media all and (min-width: 768px) and (max-width: 1050px) {
-      flex-direction: column;
-      margin-top: 3px;
-      margin: 0 auto;
-      width: 100%;
-      height: 50%;
-    }
-  `,
-  PeopleTitleView: styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-direction: column;
-    height: 100vh;
-    background-color: #505f7b;
-    color: white;
-
-    .title {
-      margin-bottom: 50px;
-      font-size: 2.5rem;
-      font-weight: bold;
-      @media all and (min-width: 768px) and (max-width: 1050px) {
-        margin-top: 20px;
-        font-size: 1rem;
-      }
-    }
-
-    .peopleTitle {
-      margin-top: 30px;
-      font-size: 1.5rem;
-      font-weight: bold;
-
-      > img {
-        width: 15px;
-        height: 15px;
-        cursor: pointer;
-      }
-    }
-    @media all and (min-width: 768px) and (max-width: 1050px) {
-      flex-direction: column;
-      margin-top: 3px;
-      margin: 0 auto;
-      width: 100%;
-      height: 50%;
-    }
-  `,
-  TitleEndView: styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-direction: column;
-    height: 50vh;
-    background-color: rgb(57 57 57);
-
-    .title {
-      position: absolute;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      font-size: 2rem;
-      font-weight: bold;
-      color: white;
-      margin-bottom: 170px;
-      /* background-color: #f0f9ff99; */
-      border-radius: 20px;
-      width: 800px;
-      height: 50px;
-      @media all and (min-width: 768px) and (max-width: 1050px) {
-        margin-top: 20px;
-        font-size: 1rem;
-      }
-    }
-    @media all and (min-width: 768px) and (max-width: 1050px) {
-      flex-direction: column;
-      margin-top: 3px;
-      margin: 0 auto;
-      width: 100%;
-      height: 200px;
-    }
-  `,
-  Image: styled.div`
-    display: flex;
-    justify-content: center;
-
-    margin-top: 100px;
-    margin-left: auto;
-    margin-right: auto;
-    img {
-      width: 30%;
-      height: 30%;
-
-      object-fit: cover;
-
-      margin-left: auto;
-      margin-right: auto;
-    }
-    .play {
-      margin-top: auto;
-      margin-left: auto;
-      margin-right: auto;
-      margin-bottom: auto;
-    }
-  `,
-  VideoContainer: styled.div`
-    /* width: 500px;
-  height: 300px; */
-    width: 50%;
-
-    img {
-      width: 100%;
-      object-fit: cover;
-      border-radius: 8px;
+    @media screen and (max-width: 780px) {
+      font-size: 1.3rem;
     }
   `,
   MainTitleView: styled.div`
@@ -260,15 +32,15 @@ flex-direction: column; */
       font-family: fantasy;
       color: white;
 
-      @media all and (min-width: 768px) and (max-width: 1050px) {
-        margin-top: 10rem;
-        width: 30%;
-        font-size: 0.5rem;
+      @media screen and (max-width: 780px) {
+        margin-top: 30%;
+        width: 65%;
+        font-size: 2rem;
       }
     }
     .icons {
       margin-top: 10%;
-      margin-left: 41%;
+      margin-left: 42%;
       margin-right: auto;
       animation-name: updown;
       animation-duration: 1s;
@@ -290,19 +62,319 @@ flex-direction: column; */
 
       :hover {
         cursor: pointer;
-        color: #6af4aa;
+        color: #b7ecf8;
         transition: all 0.3s;
+      }
+      @media screen and (max-width: 780px) {
+        width: 20%;
+        margin-left: 41%;
+        margin-right: auto;
+        margin-bottom: 25%;
       }
     }
 
     i {
       justify-content: center;
       margin-right: 50%;
-      @media all and (min-width: 768px) and (max-width: 1050px) {
+      /* @media all and (min-width: 768px) and (max-width: 1050px) {
         margin-top: 1rem;
         width: 10%;
         font-size: 0.5rem;
+      } */
+    }
+  `,
+  StartButton: styled.button`
+    /* position: absolute; */
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 1.2rem;
+    color: white;
+    width: 180px;
+    height: 70px;
+    border-style: none;
+    cursor: pointer;
+    transition: all 0.3s;
+    /* margin-left: 350px;
+  margin-right: 350px; */
+    border: 2px solid rgb(194, 194, 194);
+    border-radius: 5px;
+    margin-top: 20px;
+    margin-right: auto;
+    margin-left: auto;
+    background-color: transparent;
+
+    :hover {
+      color: #505f7b;
+      background-color: #b7ecf8;
+      transition: all 0.3s;
+    }
+    @media screen and (max-width: 780px) {
+      margin-top: 7%;
+      width: 35%;
+      height: 50px;
+      font-size: 1.2rem;
+    }
+    /* @media all and (min-width: 768px) and (max-width: 1050px) {
+      margin-top: 10rem;
+      flex-direction: column;
+      margin: 0 auto;
+
+      border: 1px solid #aaa;
+      width: 50%;
+      height: 50%;
+      font-size: 0.5rem;
+    }
+
+    @media only screen and (min-width: 768px) {
+    }
+
+    @media screen and (min-width: 480px) and (max-width: 767px) {
+    } */
+  `,
+  PopularTitleView: styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    height: 100vh;
+    margin-top: 10%;
+
+    .title {
+      font-size: 2.5rem;
+      font-weight: bold;
+      /* @media all and (min-width: 768px) and (max-width: 1050px) {
+        margin-top: 30px;
+        font-size: 0.8rem;
+      } */
+      @media screen and (max-width: 780px) {
+        font-size: 1.3rem;
       }
+    }
+
+    @media screen and (max-width: 780px) {
+      flex-direction: column;
+      /* margin-top: 20%; */
+      width: 100%;
+      height: 50%;
+    }
+  `,
+
+  FocusTitleView: styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    height: 100vh;
+
+    .title {
+      font-size: 2.5rem;
+      font-weight: bold;
+      /* @media all and (min-width: 768px) and (max-width: 1050px) {
+        margin-top: 30px;
+        font-size: 0.8rem;
+      } */
+      @media screen and (max-width: 780px) {
+        font-size: 1.3rem;
+      }
+    }
+    /* @media all and (min-width: 768px) and (max-width: 1050px) {
+      flex-direction: column;
+      margin-top: 3px;
+      margin: 0 auto;
+      width: 100%;
+      height: 50%;
+    } */
+    @media screen and (max-width: 780px) {
+      flex-direction: column;
+      /* margin-top: 20%; */
+      width: 100%;
+      height: 50%;
+    }
+  `,
+  DiyTitleView: styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    height: 100vh;
+
+    .title {
+      font-size: 2.5rem;
+      font-weight: bold;
+      /* @media all and (min-width: 768px) and (max-width: 1050px) {
+        margin-top: 20px;
+        font-size: 0.8rem;
+      } */
+      @media screen and (max-width: 780px) {
+        font-size: 1.3rem;
+        width: 90%;
+      }
+    }
+    /* @media all and (min-width: 768px) and (max-width: 1050px) {
+      flex-direction: column;
+      margin-top: 3px;
+      margin: 0 auto;
+      width: 100%;
+      height: 50%;
+    } */
+    @media screen and (max-width: 780px) {
+      flex-direction: column;
+      /* margin-top: 20%; */
+      width: 100%;
+      height: 50%;
+    }
+  `,
+  PeopleTitleView: styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    height: 100vh;
+    background-color: #505f7b;
+    color: white;
+    @media screen and (max-width: 780px) {
+      height: 600px;
+    }
+    .title {
+      margin-bottom: 50px;
+      font-size: 2.5rem;
+      font-weight: bold;
+      @media screen and (max-width: 780px) {
+        font-size: 1.3rem;
+      }
+      /* @media all and (min-width: 768px) and (max-width: 1050px) {
+        margin-top: 20px;
+        font-size: 1rem;
+      } */
+    }
+
+    .peopleTitle {
+      margin-top: 30px;
+      font-size: 1.5rem;
+      font-weight: bold;
+
+      > img {
+        width: 15px;
+        height: 15px;
+        cursor: pointer;
+      }
+    }
+
+    /* @media all and (min-width: 768px) and (max-width: 1050px) {
+      flex-direction: column;
+      margin-top: 3px;
+      margin: 0 auto;
+      width: 100%;
+      height: 50%;
+    } */
+  `,
+  TitleEndView: styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    height: 50vh;
+    background-color: rgb(57 57 57);
+    /* rgb(57 57 57) #5fa6f0*/
+    .title {
+      position: absolute;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      font-size: 2rem;
+      font-weight: bold;
+      color: white;
+      margin-bottom: 170px;
+      /* background-color: #f0f9ff99; */
+      border-radius: 20px;
+      width: 800px;
+      height: 50px;
+
+      /* @media all and (min-width: 768px) and (max-width: 1050px) {
+        margin-top: 20px;
+        font-size: 1rem;
+      } */
+      @media screen and (max-width: 780px) {
+        font-size: 1.3rem;
+        width: 90%;
+      }
+    }
+    /* @media all and (min-width: 768px) and (max-width: 1050px) {
+      flex-direction: column;
+      margin-top: 3px;
+      margin: 0 auto;
+      width: 100%;
+      height: 200px;
+    } */
+  `,
+  Image: styled.div`
+    display: flex;
+    justify-content: center;
+
+    margin-top: 5%;
+    margin-left: auto;
+    margin-right: auto;
+
+    @media screen and (max-width: 780px) {
+      width: 90%;
+      margin-bottom: 15%;
+    }
+    img {
+      width: 30%;
+      height: 30%;
+
+      object-fit: cover;
+
+      margin-left: auto;
+      margin-right: auto;
+    }
+    .play {
+      border: 4px dotted rgb(107, 217, 224);
+      border-bottom: 7px solid rgb(107, 217, 224);
+      /* border: 5px rgb(107, 217, 224) solid; */
+      margin-top: auto;
+      margin-left: 5%;
+      margin-right: auto;
+      margin-bottom: auto;
+    }
+    .play-popular {
+      border: 4px dotted #c85647;
+      border-bottom: 7px solid #c85647;
+      /* border: 5px rgb(107, 217, 224) solid; */
+      margin-top: auto;
+      margin-left: 5%;
+      margin-right: auto;
+      margin-bottom: auto;
+    }
+    .play-focus {
+      border: 4px dotted #74c983;
+      border-bottom: 7px solid #74c983;
+      /* border: 5px rgb(107, 217, 224) solid; */
+      margin-top: auto;
+      margin-left: 5%;
+      margin-right: auto;
+      margin-bottom: auto;
+    }
+    .play-diy {
+      border: 4px dotted #2c244a;
+      border-bottom: 7px solid #2c244a;
+      /* border: 5px rgb(107, 217, 224) solid; */
+      margin-top: auto;
+      margin-left: 5%;
+      margin-right: auto;
+      margin-bottom: auto;
+    }
+  `,
+  VideoContainer: styled.div`
+    /* width: 500px;
+  height: 300px; */
+    width: 50%;
+
+    img {
+      width: 80%;
+      object-fit: cover;
+      border-radius: 8px;
     }
   `,
 };
